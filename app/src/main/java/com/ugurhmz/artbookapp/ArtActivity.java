08a127b0ea4308d895e5b0dcd3b6703d87d4ca1f
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-26 02:14:44 +0300

    Image  binding with selected images

--- app/src/main/java/com/ugurhmz/artbookapp/ArtActivity.java
+++ app/src/main/java/com/ugurhmz/artbookapp/ArtActivity.java
@@ -1,27 +1,49 @@
 package com.ugurhmz.artbookapp;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.ugurhmz.artbookapp.databinding.ActivityArtBinding;
 
+
+import java.io.IOException;
+
 public class ArtActivity extends AppCompatActivity {
 
+    Bitmap selectedImage;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
     private ActivityArtBinding binding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityArtBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
 
+        registerLauncher();
     }
 
 
@@ -31,31 +53,94 @@ public class ArtActivity extends AppCompatActivity {
 
     public void selectClickImage(View view) {
 
-        // izini yoksa, kullanıcıdan Snackbar  ile izin iste
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
-                {
-                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-
-                        }
-                    }).show();
-
-                } else {
-                        // request permission
-
-                }
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view,"Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    }
+                }).show();
+            } else {
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        } else {
+            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            activityResultLauncher.launch(intentToGallery);
         }
-
-        // Kullanıcının izni zaten vardır, androidin düşük sürümleri...
-        else {  //gallery
-
-        }
-
 
     }
 
+
+
+    // registerLauncheri oluştur -> onCreate altında çağır.
+    public void registerLauncher() {
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intentFromResult = result.getData();
+                            if (intentFromResult != null) {
+                                Uri imageData = intentFromResult.getData();
+                                try {
+
+                                    if (Build.VERSION.SDK_INT >= 28) {
+                                        ImageDecoder.Source source = ImageDecoder.createSource(ArtActivity.this.getContentResolver(),imageData);
+                                        selectedImage = ImageDecoder.decodeBitmap(source);
+                                        binding.selectImage.setImageBitmap(selectedImage);
+
+                                    } else {
+                                        selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(),imageData);
+                                        binding.selectImage.setImageBitmap(selectedImage);
+                                    }
+
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                    }
+                });
+
+
+        permissionLauncher =
+                registerForActivityResult(new ActivityResultContracts.RequestPermission(),
+                        new ActivityResultCallback<Boolean>() {
+                    @Override
+                    public void onActivityResult(Boolean result) {
+                        if(result) {
+                            //izin verildi.
+                            Intent intentToGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            activityResultLauncher.launch(intentToGallery);
+
+                        } else {
+                            //izin verilmedi
+                            Toast.makeText(ArtActivity.this,"Permisson needed!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/ugurhmz/artbookapp/MainActivity.java
+++ app/src/main/java/com/ugurhmz/artbookapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ugurhmz.artbookapp.databinding.ActivityMainBinding;
 
@@ -19,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater()); ;               //Binding için
-        setContentView(binding.getRoot()); ;                                        //Binding için
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
     }
 
@@ -39,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId() == R.id.add_art){           //itemin id'si == ise art_menu.xml içindeki itemin id'sine.
-            Intent intent = new Intent(this, ArtActivity.class);        // Mainactivity'den --> ArtActiviye git. tıklanınca
+            Intent intent = new Intent(MainActivity.this, ArtActivity.class);        // Mainactivity'den --> ArtActiviye git. tıklanınca
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
