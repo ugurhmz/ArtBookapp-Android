470b4e77f4fc4d0a4e709ce8a3e8a91cbf7101e3
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-25 23:47:01 +0300

    Permission settings

--- app/src/main/java/com/ugurhmz/artbookapp/ArtActivity.java
+++ app/src/main/java/com/ugurhmz/artbookapp/ArtActivity.java
@@ -1,10 +1,15 @@
 package com.ugurhmz.artbookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.ugurhmz.artbookapp.databinding.ActivityArtBinding;
 
 public class ArtActivity extends AppCompatActivity {
@@ -25,6 +30,31 @@ public class ArtActivity extends AppCompatActivity {
     }
 
     public void selectClickImage(View view) {
+
+        // izini yoksa, kullanıcıdan Snackbar  ile izin iste
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
+                {
+                    Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    }).show();
+
+                } else {
+                        // request permission
+
+                }
+
+        }
+
+        // Kullanıcının izni zaten vardır, androidin düşük sürümleri...
+        else {  //gallery
+
+        }
+
 
     }
 
