90777a0e5e59efbc9027fd525fa8cb2909c904a4
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-12-26 14:55:00 +0300

    SQLite getDatas prepared

---
+++ app/src/main/java/com/ugurhmz/artbookapp/Art.java
@@ -0,0 +1,16 @@
+package com.ugurhmz.artbookapp;
+
+
+
+public class Art {
+
+    public String name;
+    public int id;
+
+
+    public Art(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+}
--- app/src/main/java/com/ugurhmz/artbookapp/MainActivity.java
+++ app/src/main/java/com/ugurhmz/artbookapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,9 +14,13 @@ import android.view.View;
 
 import com.ugurhmz.artbookapp.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    public ArrayList<Art> artArrayList;
 
 
     @Override
@@ -24,7 +30,36 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        artArrayList = new ArrayList<>();
+
     }
+
+
+    // Get datas
+    private void getData(){
+            try {
+
+                SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("ArtsDB",MODE_PRIVATE, null);
+
+                Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM arts", null);
+                int nameIx = cursor.getColumnIndex("art_name");
+                int idIx = cursor.getColumnIndex("id");
+
+                while(cursor.moveToNext()){
+                    String name = cursor.getString(nameIx);
+                    int id = cursor.getInt(idIx);
+                    Art art = new Art(id,name);
+                    artArrayList.add(art);
+                }
+
+                cursor.close();
+
+            } catch (Exception  e) {
+                e.printStackTrace();
+            }
+    }
+
+
 
 
     // art_menu.xml'i bağlamak
